633f72a263bb82fa01a883b8aa13c4ae8c4a9d9f
KaiD3v <kaikricardo99@gmail.com>
2026-06-09 23:05:52 -0300

    first commit

---
+++ src/main/java/br/com/raizes/RaizesApplication.java
@@ -0,0 +1,12 @@
+package br.com.raizes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaizesApplication.class, args);
+    }
+}
